a22421a30fcc5617c53333b78817ab699962b01c
Janne Lehikoinen <janne.lehikoinen@tieto.com>
2017-11-23 11:08:53 +0200

    Testing cli commands
    - WIP

--- DemoApp/DemoApp/ViewController.swift
+++ DemoApp/DemoApp/ViewController.swift
@@ -9,11 +9,78 @@
 import Cocoa
 
 class ViewController: NSViewController {
-
+    
+    // UI stuff here =>>>
+    
+    @IBOutlet weak var osLabel: NSTextField!
+    @IBOutlet var cliOutput: NSTextView!
+    
+    @IBAction func showConnectionsButtonPushed(_ sender: Any) {
+        
+        var chainedCommandResult: String?
+        
+        // 1
+        // Create example command line interface commands that needs to be run chained / piped
+        // let netstatCommand = CliCommand(launchPath: "/usr/sbin/netstat", arguments: ["-na"])
+        let netstatCommand = CliCommand(launchPath: "/bin/ls", arguments: ["-la"])
+        
+        // let grepCommand = CliCommand(launchPath: "/usr/bin/grep", arguments: ["ESTABLISHED"])
+        let grepCommand = CliCommand(launchPath: "/usr/bin/grep", arguments: ["Documents"])
+        
+        // Prepare cli command runner
+        let chainedCommand = TaskHelper(commands: [netstatCommand, grepCommand])
+        // let chainedCommand = TaskHelper(commands: [netstatCommand])
+        
+        // Execute command
+        do {
+            chainedCommandResult = try chainedCommand.execute()
+        }
+        catch _ {
+            NSLog("Failed to execute command")
+        }
+        
+        // Print to Debug area
+        print(chainedCommandResult!)
+        
+        // Add result to UI
+        cliOutput.string = chainedCommandResult!
+        
+    }
+    
+    func getOsVersion() {
+        
+        // Command line
+        var chainedCommandResult: String?
+        let swversCommand = CliCommand(launchPath: "/usr/bin/sw_vers", arguments: ["-productVersion"])
+        let chainedCommand = TaskHelper(commands: [swversCommand])
+        
+        // Execute command
+        do {
+            chainedCommandResult = try chainedCommand.execute()
+        }
+        catch _ {
+            NSLog("Failed to execute command")
+        }
+        
+        // Cocoa
+        let osVersion = ProcessInfo.processInfo.operatingSystemVersionString
+        let osVersionPieces = ProcessInfo.processInfo.operatingSystemVersion
+        let osVersionPiecesString = "\(osVersionPieces.majorVersion).\(osVersionPieces.minorVersion).\(osVersionPieces.patchVersion)"
+        
+        // Print to Debug area
+        print(osVersionPiecesString)
+        print(osVersion)
+        
+        // Add info to UI label
+        osLabel.stringValue = osVersion
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        // Get OS version when app launches
+        getOsVersion()
     }
 
     override var representedObject: Any? {
