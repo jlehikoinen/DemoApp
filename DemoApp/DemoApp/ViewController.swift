1177724534db48125a5b7c7d12a67d3fa4fd32dc
Janne Lehikoinen <janne.lehikoinen@tieto.com>
2017-11-23 09:44:28 +0200

    Demo project added

---
+++ DemoApp/DemoApp/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  DemoApp
+//
+//  Created by Janne Lehikoinen on 23/11/2017.
+//  Copyright © 2017 Janne Lehikoinen. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
